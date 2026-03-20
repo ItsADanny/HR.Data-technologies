b4d50728588afc8fe33a42a15b07b322e7d8a264
rydbl <159252313+rydbl@users.noreply.github.com>
2026-03-20 12:56:56 +0100

    header card main page

---
+++ PCweb/src/Components/Header-Component/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="headerbox">
+            <h1 className="headertext">My PC Builder</h1>
+
+            
+        </header>
+    );
+}  
--- PCweb/src/Pages/Home.tsx
+++ PCweb/src/Pages/Home.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import Header from "../Components/Header-Component/Header";
+import "../Components/Header-Component/Header.css";
 
 function Home() {
   return (
     <div className="Home">
+      <Header />
       <h1>Welcome to the Home Page!</h1>
       <p>This is the main landing page of our application.</p>
       <Link to="/about">Learn more about us</Link>
