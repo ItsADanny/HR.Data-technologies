f0b70ccddaacbdfbaa08a1893d00dc22d28fe1fd
rydbl <159252313+rydbl@users.noreply.github.com>
2026-03-20 14:08:14 +0100

    Header update link

--- PCweb/src/Components/Header-Component/Header.tsx
+++ PCweb/src/Components/Header-Component/Header.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
         <header className="headerbox">
             <h1 className="headertext">My PC Builder</h1>
 
-            
+            <Link to="/products" className="headerlink">Products</Link>
         </header>
     );
 }  
