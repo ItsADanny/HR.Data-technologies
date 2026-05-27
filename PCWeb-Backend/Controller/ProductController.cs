66051795b3f160e9878b9ffc30fab7f1bf5427cf
ItsDanny <141739582+ItsADanny@users.noreply.github.com>
2026-05-27 11:23:36 +0200

    minor commit
    Added brand search functionality

--- PCWeb-Backend/Controller/ProductController.cs
+++ PCWeb-Backend/Controller/ProductController.cs
@@ -30,6 +30,24 @@ namespace PCWeb_Backend.Controller
             return Ok(products);
         }
 
+        [HttpGet("with-category/{categoryId:int}/with-brand/{brand:alpha}")]
+        public IActionResult GetProductsWithCategory(int categoryId, string brand, int page = 1, int pageSize = 100)
+        {
+            if (categoryId <= 0)
+                return BadRequest("Invalid categoryId");
+
+            if (page < 1)
+                return BadRequest("Page must be greater than 0");
+
+            int offset = (page - 1) * pageSize;
+            var products = Product.ReadAllProductsWithCategoryWithBrand(categoryId, brand, pageSize, offset);
+            
+            if (products == null)
+                return StatusCode(500, "Error retrieving products from database");
+
+            return Ok(products);
+        }
+
         [HttpGet("{id}")]
         public IActionResult GetProduct(int id)
         {
