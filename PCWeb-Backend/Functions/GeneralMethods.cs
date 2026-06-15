747bacb53b3964f0a2dddc8e7325583e6445a240
hengyi2002 <hengyi2002@hotmail.com>
2026-06-15 14:21:57 +0200

    +fix 2

--- PCWeb-Backend/Functions/GeneralMethods.cs
+++ PCWeb-Backend/Functions/GeneralMethods.cs
@@ -2,6 +2,6 @@ public static class GeneralMethods
 {
     public static bool IntToBool(int input) => input == 1;
     public static int BoolToInt(bool input) => input ? 1 : 0;
-    public static DateTime ParseDBDateTime(string dbDateTimeString) => DateTime.Parse(dbDateTimeString, System.Globalization.CultureInfo.InvariantCulture);
+    public static DateTime ParseDBDateTime(string dbDateTimeString) => DateTime.Parse(dbDateTimeString);
     public static string SQLInjectionSanitizer(string input) => input.Replace("'", "''");
 }
