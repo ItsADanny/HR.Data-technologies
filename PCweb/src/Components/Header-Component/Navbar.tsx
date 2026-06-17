612bb766d1981fbcdd2165e9da7d6b0a2af1cf96
HY Ke <57900493+hengyi2002@users.noreply.github.com>
2026-06-17 16:49:16 +0200

    navi

--- PCweb/src/Components/Header-Component/Navbar.tsx
+++ PCweb/src/Components/Header-Component/Navbar.tsx
@@ -1,47 +1,53 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 export default function Navbar() {
     const [open, setOpen] = useState(false);
 
-    const closeDropdown = () => {
-        setOpen(false);
-    };
-
     return (
-        <header className='navbar-shell'>
-            <div className='navbar-container'>
-                <div
-                    className={`category-dropdown ${open ? 'open' : ''}`}
-                    onMouseEnter={() => setOpen(true)}
-                    onMouseLeave={closeDropdown}
-                >
-                    <button className='category-trigger'>
-                        Category
-                    </button>
+        <nav className='navbar-container' onMouseLeave={() => setOpen(false)}>
+            <div
+                className={`category-dropdown ${open ? 'open' : ''}`}
+                onMouseEnter={() => setOpen(true)}
+            >
+                <button className='category-trigger'>
+                    Category ▾
+                </button>
 
-                    <ul className='category-menu'>
-                        <li className='has-submenu'>
-                            <a href='/'>PC Components</a>
-                            <ul className='subcategory-menu'>
-                                <li><a href='/'>CPUs</a></li>
-                                <li><a href='/'>GPUs</a></li>
-                                <li><a href='/'>RAM</a></li>
-                                <li><a href='/'>Motherboards</a></li>
-                                <li><a href='/'>Power Supplies</a></li>
+                {open && (
+                    <div className='megamenu'>
+                        <div className='megamenu-section'>
+                            <h3 className='megamenu-heading'>PC Components</h3>
+                            <ul className='megamenu-list'>
+                                <li><Link to='/viewproducts?categoryId=4' onClick={() => setOpen(false)}>CPUs</Link></li>
+                                <li><Link to='/viewproducts?categoryId=15' onClick={() => setOpen(false)}>GPUs</Link></li>
+                                <li><Link to='/viewproducts?categoryId=8' onClick={() => setOpen(false)}>RAM / Memory</Link></li>
+                                <li><Link to='/viewproducts?categoryId=9' onClick={() => setOpen(false)}>Motherboards</Link></li>
+                                <li><Link to='/viewproducts?categoryId=12' onClick={() => setOpen(false)}>Power Supplies</Link></li>
+                                <li><Link to='/viewproducts?categoryId=7' onClick={() => setOpen(false)}>Storage</Link></li>
+                                <li><Link to='/viewproducts?categoryId=5' onClick={() => setOpen(false)}>Cooling</Link></li>
+                                <li><Link to='/viewproducts?categoryId=1' onClick={() => setOpen(false)}>Cases</Link></li>
                             </ul>
-                        </li>
-                        <li><a href='/'>Gaming PCs</a></li>
-                        <li><a href='/'>Laptops</a></li>
-                        <li><a href='/'>Monitors</a></li>
-                        <li><a href='/'>Storage</a></li>
-                        <li><a href='/'>Accessories</a></li>
-                    </ul>
-                </div>
-                <a href="/">Sales</a>
-                <a href="/">Contact</a>
-                <Link className='navbar-partpicker-btn' to='/partpicker'>⚙ Build Your PC</Link>
+                        </div>
+
+                        <div className='megamenu-divider' />
+
+                        <div className='megamenu-section'>
+                            <h3 className='megamenu-heading'>Browse</h3>
+                            <ul className='megamenu-list'>
+                                <li><Link to='/viewproducts' onClick={() => setOpen(false)}>All Products</Link></li>
+                                <li><Link to='/partpicker' onClick={() => setOpen(false)}>PC Builder</Link></li>
+                                <li><Link to='/cart' onClick={() => setOpen(false)}>Cart</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                )}
             </div>
-        </header>
+
+            <Link to='/viewproducts' className='navbar-link'>Shop</Link>
+            <Link to='/contact' className='navbar-link'>Contact</Link>
+            <Link className='navbar-partpicker-btn' to='/partpicker'>⚙ Build Your PC</Link>
+        </nav>
     );
 }
